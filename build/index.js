4f4e05f7ff96b91a049e8c5ce4e6059b68f4b07d
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2023-01-27 00:51:17 +0530

    cold reload, prod build/script

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hellow Typescript");
+(() => {
+    let i = 0;
+    console.log(i);
+    i++;
+    console.log(i);
+})();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("Hellow Typescript");
+
+(() => {
+  let i = 0;
+  console.log(i);
+  i++;
+  console.log(i);
+})();
